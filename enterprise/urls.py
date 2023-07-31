15985d56a0b1e31b3b7d4092d3f0a50df4e4f83a
SpaRtaleon <80996846+SpaRtaleon@users.noreply.github.com>
2023-07-31 23:39:47 +0530

    Update urls.py

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
 
     path("", include("recipe.urls")),
     
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static('/static/', document_root=os.path.join(BASE_DIR, 'static')
