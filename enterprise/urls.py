771a91bd3b10a7e095e321d6e709ba0d43657def
SpaRtaleon <80996846+SpaRtaleon@users.noreply.github.com>
2023-07-31 23:47:03 +0530

    Update urls.py

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("", include("recipe.urls")),
     
-] + static('/static/', document_root=os.path.join(BASE_DIR, 'static')
+]
