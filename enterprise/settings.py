125b7931718beab5c1027332af0658fa0284fe2f
SpaRtaleon <raghubalan2972001@gmail.com>
2023-09-18 08:00:29 +0530

    staticfiles for production

--- enterprise/settings.py
+++ enterprise/settings.py
@@ -163,9 +163,10 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #for production
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles', 'static')#for production
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
