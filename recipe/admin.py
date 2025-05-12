4d7b567c9a444e3f0417d47612f2986cf672dbaf
SpaRtaleon <80996846+SpaRtaleon@users.noreply.github.com>
2025-05-12 13:05:49 +0530

    Update admin.py

--- recipe/admin.py
+++ recipe/admin.py
@@ -7,7 +7,7 @@ from .models import Recipe,Category,User,UserFavorites,PopularRecipe,Ingredient,
 
 # Register your models here.
 admin.site.register(Recipe)
-admin.site.register(Category)
+admin.site.register(Category) 
 admin.site.register(User)
 admin.site.register(UserFavorites)
 admin.site.register(PopularRecipe)
